1916ba60cd07fcd83ef25521d81905a861f47988
Shlomo Kalinsky <93456838+sdkkds2125@users.noreply.github.com>
2023-09-27 18:17:10 -0400

    added name in comment

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,11 +13,15 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+//Shlomo Kalinsky
 namespace PresidentEligibilityWPF_GUI_Test
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
